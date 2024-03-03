391170658c8b8b75abedfcf5eaafac3878611d52
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-03-04 00:28:29 +0100

    add new problem

---
+++ vector/solution.cpp
@@ -0,0 +1,23 @@
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+#include <iostream>
+#include <vector>
+using namespace std;
+int sum(vector <int> numbers){
+    int sum = 0;
+    for(int i=0; i<numbers.size(); i++){
+        if(numbers[i] < 0){
+                sum +=  0;
+        }else{
+                sum +=  numbers[i];
+            }
+    }
+    numbers.push_back(sum);
+    return sum;
+}
+int main() {
+    vector<int> numbers = {1, 7, 12, -4};
+    cout<<sum(numbers);
+    
+
+    return 0;
+}
