8c2d5954268063cf09fb8a4894860389cc49ea97
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-03-26 20:42:19 +0100

    add new problem

---
+++ codeforces/magnit.cpp
@@ -0,0 +1,25 @@
+// link ==> https://codeforces.com/contest/344
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string mag;
+    int number, count = 1;
+ 
+    cin>>number;
+    for(int i=0; i<number; i++){
+        string value;
+        cin>>value;
+        if(i == 0){
+            mag = value;
+            continue;
+        }
+        if(mag[1] == value[0]){
+            count++;
+        }
+        mag = value;
+    }
+    cout<<count;
+    return 0;
+}
---
+++ pass.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <iostream>
+#include <string>
+
+
+
+
+
+int main()
+{
+    cout<<"enter the number ";
+    int n;
+    cin>>n;
+    
+    
+   
+}
